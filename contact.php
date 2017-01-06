396cc868d4400dba434f072d1b4e4b9465486a68
DESKTOP-ELIGO1N\Richi <richinel_jarbandhan@hotmail.com>
2017-01-06 15:34:36 +0100

    no message

--- contact.php
+++ contact.php
@@ -12,31 +12,42 @@
 
 			<p>
 				<strong>Encaasa BV</strong><br>
-				Mijnsherenaan 216<br>
+				Mijnsherenlaan 216<br>
 				3083 BZ Rotterdam<br>
 				 <br>
 				 
 				KVK : 58872566<br>
 				<br>
 				 
-				Ook kunt u ons via onderstaand formulier mailen u kunt hier een dag en tijd opgeven waarop wij u kunnen bellen (terug bellen kan tot 20:00 uur)
+				Ook kunt u ons via onderstaand formulier mailen. U kunt hier een dag en tijd opgeven waarop wij u kunnen bellen (Terug bellen kan tot 20:00 uur).
 			</p>
 
 			<form action="./mail_function.php" id="c_form">
+
+				<!-- <?php if(isset($_GET['CaptchaFail'])){ ?>
+					<div>Verifieer alstublieft de ReCaptcha!</div>
+				<?php } ?> -->
+
 				<input type="text" name="name" placeholder="Voornaam + Achternaam" required>
-				<br>
-				<br>
+					<br>
+					<br>
 				<input type="text" name="email" placeholder="E-mailadres" required>
-				<br>
-				<br>
+					<br>
+					<br>
 				<input type="text" name="subject" placeholder="Onderwerp" required>
-				<br>
-				<br>
+					<br>
+					<br>
 				<textarea name="msg" placeholder="Uw bericht" required></textarea>
-				<br>
-				<br>
+					<br>
+					<br>
+				<div class="g-recaptcha" data-sitekey="6LcKhBAUAAAAAPwH5jso0Ujzx-UmmxutRvgetQmE"></div>
+					<br>
+					<br>
 				<button type="submit" name="submit" value="Verzenden"><i class="fa fa-paper-plane-o" aria-hidden="true"></i> Verzenden</button>
 			</form>
+
+			<script src='https://www.google.com/recaptcha/api.js'></script>
+
 		</div>
 
 		<?php include("includes/direct_help.php");?>
--- mail_function.php
+++ mail_function.php
@@ -1,25 +1,18 @@
-<html>
-<head><title>Mail</title><link rel="stylesheet" type="text/css" href="css/main.css"></head>
-<body>
 <?php
 
-// mailfunction
+	// mailfunction
+	$name 		= $_POST["name"];
+	$email 		= $_POST["email"];
+	$subject 	= $_POST["subject"];
+	$msg 		= $_POST["msg"];
+	$mymail 	= "rj_1993@live.nl";
+	$DATA 		= "$name | $email\r\n$msg";
+	mail($mymail, "Portfolio", $DATA);
 
-$name 		= $_POST["name"];
-$email 		= $_POST["email"];
-$subject 	= $_POST["subject"];
-$msg 		= $_POST["msg"];
-$mymail 	= "rj_1993@live.nl";
-$DATA 		= "$name | $email\r\n$msg";
-mail($mymail, "Portfolio", $DATA);
+	echo "Wij hebben uw e-mail in goede orde ontvangen! We zullen zo gauw mogelijk contact met u opnemen! <br><br>Met vriendelijke groet,<br> Encaasa";
+	header('Refresh: 10; url=index.php'); 
 
-// forward
-echo "Wij hebben uw e-mail in goede orde ontvangen! We zullen zo gauw mogelijk contact met u opnemen! <br><br>Met vriendelijke groet,<br> Encaasa";
-header('Refresh: 10; url=index.php'); 
-
+	echo '<a href="index.php">Direct terugkeren</a>';
 ?>
 
-<a href="index.php">Direct terugkeren</a>
 
-</body>
-</html>
